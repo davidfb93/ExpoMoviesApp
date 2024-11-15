e4cde7e772e68c7e2c5739e594e7e1a869d0cdf3
david.banol.3889@eam.edu.co <david.banol.3889@eam.edu.co>
2024-11-15 03:08:14 -0500

    Version 1.0.0 Expo MoviesApp

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -7,12 +7,13 @@ import { useMovies } from '@/presentation/hooks/useMovies'
 import MainSlideshow from '@/presentation/components/movies/MainSlideshow';
 import MovieHorizontalList from '@/presentation/components/movies/MovieHorizontalList';
 import SearchScreen from '@/presentation/components/movies/searchscreen';
+import { Ionicons } from '@expo/vector-icons';
 
 
 const HomeScreen = () => {
 
   const safeArea = useSafeAreaInsets();
-  const {  nowPlayingQuery, popularQuery, topRatedQuery, upcomingQuery } = useMovies();
+  const { nowPlayingQuery, popularQuery, topRatedQuery, upcomingQuery } = useMovies();
 
   if (nowPlayingQuery.isLoading) {
 
@@ -27,27 +28,35 @@ const HomeScreen = () => {
   return (
 
     <ScrollView>
-    <View style={{ marginTop:2, paddingBottom: 25 ,paddingTop: safeArea.top, backgroundColor: '#14161E' }}>
-      <Text className='text-3xl font-bold px-4 mb-2 mt-4 color-white'>MoviesApp Expo</Text>
+      <View style={{ marginTop: 2, paddingBottom: 25, paddingTop: safeArea.top, backgroundColor: '#14161E' }}>
+        <View className="flex-row items-center p-4">
+          <Ionicons
+            name="film-outline"
+            size={50}
+            color="white"
+            className="shadow ml-5"
+          />
+          <Text className="text-3xl font-bold px-4 color-white">MoviesApp Expo</Text>
+        </View>
 
-      <SearchScreen />
+        <SearchScreen />
 
-      {/* Carrusel principal - En cartelera - Uso de librerias externas */}
-      <Text style = {styles.title}>En cartelera</Text>
-      <MainSlideshow movies={ nowPlayingQuery.data ?? []} />
+        {/* Carrusel principal - En cartelera - Uso de librerias externas */}
+        <Text style={styles.title}>En cartelera</Text>
+        <MainSlideshow movies={nowPlayingQuery.data ?? []} />
 
-      {/* Flatlist peliculas popular - requisito técnico */}
-      < MovieHorizontalList title = 'Populares' movies={ popularQuery.data ?? []} />
+        {/* Flatlist peliculas popular - requisito técnico */}
+        < MovieHorizontalList title='Populares' movies={popularQuery.data ?? []} />
 
-      < MovieHorizontalList 
-        title = 'Mejor Calificadas' 
-        movies={ topRatedQuery.data?.pages.flat() ?? []} 
-        loadNextPage={ topRatedQuery.fetchNextPage }
+        < MovieHorizontalList
+          title='Mejor Calificadas'
+          movies={topRatedQuery.data?.pages.flat() ?? []}
+          loadNextPage={topRatedQuery.fetchNextPage}
         />
 
-      < MovieHorizontalList title = 'Próximamente en cines' movies={ upcomingQuery.data ?? []} />
+        < MovieHorizontalList title='Próximamente en cines' movies={upcomingQuery.data ?? []} />
 
-    </View>
+      </View>
     </ScrollView>
   )
 }
--- presentation/components/movies/MovieHorizontalList.tsx
+++ presentation/components/movies/MovieHorizontalList.tsx
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     paddingBottom: 10
   },
   flatlist: {
-    paddingBottom: 20
+    paddingBottom: 20,  
   }
 })
 
